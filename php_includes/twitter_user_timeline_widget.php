13edb6087028ed1a8d7c0f5173099e23f15d0aae
Neelabh Gupta <gupta.neelabh@gmail.com>
2014-09-22 05:56:59 -0500

    Add Twitter timeline widget as a PHP include file

---
+++ php_includes/twitter_user_timeline_widget.php
@@ -0,0 +1,2 @@
+<a class="twitter-timeline" href="https://twitter.com/neelabhg" data-widget-id="514004634839437312">Tweets by @neelabhg</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
